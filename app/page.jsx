6be2d1302be4db3dfc44006d3285bd25c698978a
victor <victor.lvl47@gmail.com>
2023-08-20 22:33:18 -0600

    added a nav bar

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Feed from '@components/Feed';
 
 const Home = () => {
     return (
@@ -12,7 +13,7 @@ const Home = () => {
                 discover, create and share creative prompts.
             </p>
 
-            {/* Feed */}
+            < Feed />
         </section>
     )
 }
