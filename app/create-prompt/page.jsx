4c9d191572feceebc0128e6f345230b62324f1b9
victor <victor.lvl47@gmail.com>
2023-08-30 00:23:36 -0600

    create the create-prompt route

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,11 @@
+const CreatePrompt = () => {
+    return (
+        <div>
+            CreatePrompt
+        </div>
+    )
+}
+
+export default CreatePrompt
+
+{ /* https://www.youtube.com/watch?v=wm5gMKuwSYk&t=6364s timestamp 1:49:43*/}
