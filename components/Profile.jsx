34c0b56ee470487ee3461d6b22ceff8a822ad94e
victor <victor.lvl47@gmail.com>
2023-09-09 22:54:37 -0600

    added title and description to the profile page

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,6 +1,15 @@
-const Profile = () => {
+import PromptCard from './PromptCard';
+
+const Profile = ({name, desc, data, handleEdit, handleDelete}) => {
     return (
-        <div>Profile</div>
+        <section className="w-full;">
+            <h1 className="head_text text-left">
+                <span className="blue_gradient">
+                    {name} Profile
+                </span>
+            </h1>
+            <p className="desc text-left">{desc}</p>
+        </section>
     )
 }
 
