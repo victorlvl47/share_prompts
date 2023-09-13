b070fcbbd69c9b7a130ba8061bcab0de31455b80
victor <victor.lvl47@gmail.com>
2023-09-12 18:53:29 -0600

    added delete and edit options for the prompt the user created

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,6 +7,10 @@ import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
 
+    const { data: session } = useSession();
+    const pathName = usePathname();
+    const router = useRouter();
+
     const [copied, setCopied] = useState("");
 
     const handleCopy = () => {
@@ -56,8 +60,23 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
                onClick={() => handleTagClick && handleTagClick(post.tag)}>
                 {post.tag}
             </p>
+
+            {post.creator && session?.user.id === post.creator._id && pathName === '/profile' && (
+                <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+                    <p className='font-inter text-sm green_gradient cursor-pointer'
+                       onClick={handleEdit}>
+                        Edit
+                    </p>
+                    <p className='font-inter text-sm orange_gradient cursor-pointer'
+                       onClick={handleDelete}>
+                        Delete
+                    </p>
+                </div>
+            )}
         </div>
     )
 }
 
 export default PromptCard;
+
+{/* https://www.youtube.com/watch?v=wm5gMKuwSYk&t=9893s  2:47:36 */}
